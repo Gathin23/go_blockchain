8aeb4d9cbcdb7a883f50f1654016d116538423af
Gathin <gathint23@gmail.com>
2023-08-04 06:54:28 +0530

    Creating block

--- blockchain.go
+++ blockchain.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
-	"log")
+	"log"
+	"time"
+)
 
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+
+	return b
+}
+
+//override function to create printing format for blocks
+func (b *Block) Print() {
+	fmt.Printf("Timestamp        %d\n",b.timestamp)
+	fmt.Printf("Nonce            %d\n",b.nonce)
+	fmt.Printf("Previous_Hash    %s\n",b.previousHash)
+	fmt.Printf("Transactions     %s\n",b.transactions)
+}
 
 
 func init() {
@@ -11,6 +36,6 @@ func init() {
 }
 
 func main() {
-	log.Println("Test")
-	fmt.Printf("Test2")
+	b := NewBlock(0, "init hash")
+	b.Print()
 }
